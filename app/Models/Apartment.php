73130dd93928f8ae46a436c3cd4b205b0d6d0970
MindulaHewadewage <mindufernando@gmail.com>
2023-10-25 13:19:03 +0200

    bug fix

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Apartments extends Model
+class Apartment extends Model
 {
     use HasFactory;
     protected $fillable = [
--- app/Models/Sponsorship.php
+++ app/Models/Sponsorship.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sponsorships extends Model
+class Sponsorship extends Model
 {
     use HasFactory;
 
