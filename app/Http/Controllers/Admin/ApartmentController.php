e156e8990206c23a27f911ed981f7f0735f916c8
Andrea Fermo <andreafermo.sd@gmail.com>
2023-10-26 12:59:18 +0200

    rotte e controller apartments

---
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+use App\http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $apartments = Apartment::where('user_id', Auth::user()->id)->orderBy('name', 'asc')->get();
+        return view('admin.apartments.index', compact('apartments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.apartments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request['slug'] = Apartment::generateSlug($request->name) . - . Auth::user()->id;
+
+        $request['apartment_id'] = Auth::user()->id;
+
+        $newApartment = Apartment::create($request);
+
+        return redirect()->route('admin.apartments.show', ['apartment' => $newApartment->slug]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Apartment $apartment)
+    {
+        if (Auth::user()->id == $apartment->user_id){
+            return view('admin.apartments.show', compact('apartment'));
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Apartment $apartment)
+    {
+        if (Auth::user()->id == $apartment->user_id) {
+            return view('admin.apartments.edit', compact('apartment'));
+        } 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Apartment  $apartments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Apartment $apartment)
+    {
+        $request['slug'] = Apartment::generateSlug($request->name);
+
+        $request['slug'] = $request['slug'] . '-' . $apartment->id;
+
+        $apartment->update($request);
+
+        return redirect()->route('admin.apartments.show', ['apartment' => $apartment->slug]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Apartment  $apartments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Apartment $apartment)
+    {
+       
+        $apartment->delete();
+        return redirect()->route('admin.apartments.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\Admin\ApartmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +19,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function() {
+   Route::resource('apartments', ApartmentController::class)->parameters(['apartments' => 'apartment:slug']); 
 });
 
 require __DIR__.'/auth.php';
